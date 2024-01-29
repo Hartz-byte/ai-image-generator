5cc4492c280f64d7d2043a1f9595dc9be6caa09a
Hartz-byte <harshmail281199@gmail.com>
2024-01-29 19:30:24 +0530

    project setup

---
+++ src/Components/ImageGenerator/ImageGenerator.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './ImageGenerator.css'
+
+const ImageGenerator = () => {
+  return (
+    <div>
+      ImageGenerator
+    </div>
+  )
+}
+
+export default ImageGenerator
