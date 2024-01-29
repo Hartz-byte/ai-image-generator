949eef24012fa922460222065d37f7c7218462ed
Hartz-byte <harshmail281199@gmail.com>
2024-01-29 19:54:54 +0530

    styling done

--- src/Components/ImageGenerator/ImageGenerator.js
+++ src/Components/ImageGenerator/ImageGenerator.js
@@ -1,12 +1,33 @@
-import React from 'react'
-import './ImageGenerator.css'
+import React from "react";
+import "./ImageGenerator.css";
+import default_image from "../Assets/default_image.svg";
 
 const ImageGenerator = () => {
   return (
-    <div>
-      ImageGenerator
-    </div>
-  )
-}
+    <div className="main-container">
+      {/* heading */}
+      <div className="header">
+        AI Image <span>Generator</span>
+      </div>
 
-export default ImageGenerator
+      {/* image container and loading */}
+      <div className="image-loading">
+        <div className="image">
+          <img src={default_image} alt="default image" />
+        </div>
+      </div>
+
+      {/* search button */}
+      <div className="search-box">
+        <input
+          type="text"
+          className="search-input"
+          placeholder="Describe what you want to see?"
+        />
+        <div className="generate-btn">Generate</div>
+      </div>
+    </div>
+  );
+};
+
+export default ImageGenerator;
